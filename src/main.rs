f811410b76e9d8546bec3869b6b2988b1caed1ef
mfloto <60036186+mfloto@users.noreply.github.com>
2023-05-17 01:42:26 +0200

    chore: Cleanup main and Cargo.toml
    Co-authored-by: Flippchen <91947480+flippchen@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod typechecker;
 mod types;
 
-use serde::{Deserialize, Serialize};
-use serde_json;
 use std::error::Error;
-use std::fs::File;
-use std::io::{Read, Write};
 use tracing::info;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt::init();
+    info!("Hello RustyJ!");
+    lib::hi();
     Ok(())
 }
