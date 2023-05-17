e5e0f4c532078411286c37569a774a02f2c059fd
sander <sander.stella00@gmail.com>
2023-05-17 22:17:58 +0200

    added a first test exampel to get familar with pest.rs example not working Co-authored-by: tori <v.goennheimer@benz24.de>
    Signed-off-by: sander <sander.stella00@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 mod parser;
 
+use parser::Example;
 use tracing::info;
+use crate::parser::parse_Example;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     tracing_subscriber::fmt::init();
     info!("Hello RustyJ!");
+
+
+    // let unparesed_file =
+    let test:  Example = parse_Example("Sander,Stella\nTori,Gönnung").expect("u suck");
+    println!("{}", serde_json::to_string(&test).unwrap());
+
+
     lib::hi();
     Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,12 @@
 use pest::Parser;
+use pest::error::Error;
+use pest::iterators::Pair;
 use pest_derive::Parser;
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
 
 //  Name, Name , Name
 
@@ -7,7 +14,31 @@ use pest_derive::Parser;
 #[grammar = "JavaGrammar.pest"]
 struct JavaParser;
 
+#[derive(Debug, Clone, Deserialize, Serialize , Disply)]
+pub enum Example
+{
+    ID(* str),
+    Name(Example::ID(), Example::ID()),
+    List([Name]),
+}
 
+
+pub fn parse_Example(file: &str) -> Result<Example, Error<Rule>>
+{
+    let expamle = JavaParser::parse(Rule::List, file)?.next().unwrap();
+    use pest::iterators::Pair;
+
+    fn parse_value(pair: Pair<Rule>) -> Example
+    {
+        match pair.as_rule() {
+            Rule::ID => Example::ID(pair.into_inner().next().unwrap().as_str()),
+            Rule::Name => Example::Name(pair.into_inner().next().unwrap(), pair.into_inner().next().unwrap()),
+            Rule::List => Example::List(pair.into_inner().map(parse_value).collect()),
+        }
+    }
+
+    Ok(parse_value(expamle))
+}
 
 
 
