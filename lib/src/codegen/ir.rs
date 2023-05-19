269ada0b0c34e4abb7ad773d6fcfac50ae5f3418
Marion Hinkel <marion@fauxia.de>
2023-05-19 15:55:14 +0200

    added my functions back in

--- lib/src/codegen/ir.rs
+++ lib/src/codegen/ir.rs
@@ -65,3 +65,107 @@ fn generate_method(method: &MethodDecl, dir: &mut DIR) -> CompiledMethod {
 fn generate_stmt(stmt: Stmt, dir: &mut DIR) -> Vec<Instruction> {
     todo!()
 }
+
+fn generate_code_stmt(stmt: &Stmt, code: &mut Vec<u8>) {
+    match stmt {
+        Stmt::Block(stmts) => {
+            // Generate bytecode for block
+        }
+        Stmt::Return(expr) => {
+            // Generate bytecode for return
+        }
+        Stmt::While(expr, stmt) => {
+            // Generate bytecode for while
+        }
+        Stmt::LocalVarDecl(types, name) => {
+            // Generate bytecode for local var decl
+        }
+        Stmt::If(expr, stmt1, stmt2) => {
+            // Generate bytecode for if
+        }
+        Stmt::StmtExprStmt(stmt_expr) => {
+            // Generate bytecode for stmt expr
+        }
+        Stmt::TypedStmt(stmt, types) => {
+            // Generate bytecode for typed stmt
+        }
+    }
+}
+
+fn generate_code_stmt_expr(stmt_expr: &StmtExpr, code: &mut Vec<u8>) {
+    match stmt_expr {
+        StmtExpr::Assign(name, expr) => {
+            // Generate bytecode for assignment
+        }
+        StmtExpr::New(types, exprs) => {
+            // Generate bytecode for new
+        }
+        StmtExpr::MethodCall(expr, name, exprs) => {
+            // Generate bytecode for method call
+        }
+        StmtExpr::TypedStmtExpr(stmt_expr, types) => {
+            // Generate bytecode for typed stmt expr
+        }
+    }
+}
+
+fn generate_code_expr(expr: &Expr, code: &mut Vec<u8>) {
+    match expr {
+        Expr::This => {
+            // Generate bytecode for this
+        }
+        Expr::Super => {
+            // Generate bytecode for super
+        }
+        Expr::LocalOrFieldVar(name) => {
+            // Generate bytecode for local or field var
+        }
+        Expr::InstVar(expr, name) => {
+            // Generate bytecode for inst var
+        }
+        Expr::Unary(op, expr) => {
+            // Generate bytecode for unary
+        }
+        Expr::Binary(op, expr1, expr2) => {
+            // Generate bytecode for binary
+        }
+
+        Expr::Integer(value) => {
+            // Generate bytecode for int literal
+        }
+        Expr::Bool(value) => {
+            // Generate bytecode for bool literal
+        }
+        Expr::Char(value) => {
+            // Generate bytecode for char literal
+        }
+        Expr::String(value) => {
+            // Generate bytecode for string literal
+        }
+        Expr::Jnull => {
+            // Generate bytecode for null literal
+        }
+        Expr::StmtExprExpr(stmt_expr) => {
+            // Generate bytecode for stmt expr
+        }
+        Expr::TypedExpr(expr, types) => {
+            // Generate bytecode for typed expr
+        }
+    }
+}
+fn generate_code_type(types: &Type, code: &mut Vec<u8>) {
+    match types {
+        Type::Int => {
+            // Generate bytecode for int
+        }
+        Type::Bool => {
+            // Generate bytecode for bool
+        }
+        Type::Char => {
+            // Generate bytecode for char
+        }
+        Type::String => {
+            // Generate bytecode for string
+        }
+    }
+}
