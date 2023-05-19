9ea8c73b14e55cb3b8a05b903837dd4399d69bb1
Marion Hinkel <marion@fauxia.de>
2023-05-19 15:35:32 +0200

    function structure

---
+++ lib/src/codegen/ir.rs
@@ -0,0 +1,91 @@
+use crate::typechecker::*;
+use crate::types::*;
+
+pub fn generate_code(ast: &Prg) -> Vec<u8> {
+    let mut code = Vec::new();
+    generate_code_recursive(&ast, &mut code);
+    code
+}
+
+fn generate_code_recursive(node: &Prg, code: &mut Vec<u8>) {
+    match node {
+        Prg::Expr(expr) => generate_code_expr(expr, code),
+        Prg::Stmt(stmt) => generate_code_stmt(stmt, code),
+        Prg::Field(field) => generate_code_field(field, code),
+        Prg::Type(types) => generate_code_type(types, code),
+        Prg::MethodDecl(methoddecl) => generate_code_methoddecl(methoddecl, code),
+        Prg::FieldDecl(fielddecl) => generate_code_fielddecl(fielddecl, code),
+    }
+}
+
+fn generate_code_expr(expr: &Expr, code: &mut Vec<u8>) {
+    match expr {
+        Expr::IntLiteral(value) => {
+            // Generate bytecode for int literal
+        }
+        Expr::BoolLiteral(value) => {
+            // Generate bytecode for bool literal
+        } // ...
+    }
+}
+
+fn generate_code_stmt(stmt: &Stmt, code: &mut Vec<u8>) {
+    match stmt {
+        Stmt {} => {
+            // Generate bytecode for if statement
+        } // ...
+    }
+}
+fn generate_code_stmt_expr(stmt_expr: &StmtExpr, code: &mut Vec<u8>) {
+    match stmt {
+        Stmt {} => {
+            // Generate bytecode for if statement
+        } // ...
+    }
+}
+fn generate_code_field(field: &Field, code: &mut Vec<u8>) {
+    match field {
+        Field::FieldDecl {
+            name,
+            field_type,
+            value,
+        } => {
+            // Generate bytecode for field declaration
+        } // ...
+    }
+}
+fn generate_code_type(types: &Type, code: &mut Vec<u8>) {
+    match types {
+        Method::MethodDecl {
+            name,
+            ret_type,
+            params,
+            body,
+        } => {
+            // Generate bytecode for method declaration
+        } // ...
+    }
+}
+fn generate_code_methoddecl(methoddecl: &MethodDecl, code: &mut Vec<u8>) {
+    match methoddecl {
+        MethodDecl::MethodDecl {
+            name,
+            ret_type,
+            params,
+            body,
+        } => {
+            // Generate bytecode for method declaration
+        } // ...
+    }
+}
+fn generate_code_fielddecl(fielddecl: &FieldDecl, code: &mut Vec<u8>) {
+    match fielddecl {
+        FieldDecl::FieldDecl {
+            name,
+            field_type,
+            value,
+        } => {
+            // Generate bytecode for field declaration
+        } // ...
+    }
+}
--- lib/src/codegen/mod.rs
+++ lib/src/codegen/mod.rs
@@ -1,3 +1,4 @@
+mod ir;
 /// The DIR(Duck Intermediate Representation) is our IR for generating Java Bytecode
 /// from our TAST
 struct DIR {}
