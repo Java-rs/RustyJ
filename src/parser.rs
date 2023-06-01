e9dbdb305cf2bfdf9af1ec496a49541c756c19ca
sander <sander.stella00@gmail.com>
2023-06-01 15:25:50 +0200

    removed left recusion Co-authored-by: ArtInLines <linesinart@gmail.com> tori <v.goennheimer@benz24.de>
    Signed-off-by: sander <sander.stella00@gmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -5,6 +5,8 @@ use pest::error::Error;
 use pest::Parser;
 use pest_derive::Parser;
 use serde::{Deserialize, Serialize};
+use pest::iterators::Pair;
+
 
 #[derive(Parser)]
 #[grammar = "JavaGrammar.pest"]
@@ -20,20 +22,21 @@ pub enum Example {
 pub fn parse_Example(file: &str) -> Result<Example, Error<Rule>> {
     let example: Pair<Rule> = ExampleParser::parse(Rule::List, file)?.next().unwrap();
 
-    use pest::iterators::Pair;
 
     fn parse_value(pair: Pair<Rule>) -> Example {
         match pair.as_rule() {
             Rule::ID => Example::ID(String::from(pair.as_str())),
-            Rule::Name => {
-                let mut pairs = pair.into_inner();
-                let a = parse_value(pairs.next().unwrap());
-                let b = parse_value(pairs.next().unwrap());
-                Example::Name(Box::new(a), Box::new(b))
-            }
+            Rule::Name => Name(pair: Pair<Rule>),
             Rule::List => Example::List(pair.into_inner().map(parse_value).collect()),
         }
     }
 
     Ok(parse_value(example))
+}
+pub fn Name(pair: Pair<Rule>)->Example::Name
+{
+    let mut pairs = pair.into_inner();
+    let a = parse_value(pairs.next().unwrap());
+    let b = parse_value(pairs.next().unwrap());
+  return  Example::Name(Box::new(a), Box::new(b))
 }
