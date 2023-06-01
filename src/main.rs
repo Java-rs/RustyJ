1cdae16e51b26484457c133eac59b013fb6566f1
sander <sander.stella00@gmail.com>
2023-06-01 16:08:33 +0200

    Moved files back into incorrect folder Co-authored-by: ArtInLines <linesinart@gmail.com> tori <v.goennheimer@benz24.de>
    Signed-off-by: sander <sander.stella00@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-mod parser;
-
 use parser::Example;
 use parser::*;
 use tracing::info;
@@ -8,13 +6,6 @@ fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     tracing_subscriber::fmt::init();
     info!("Hello RustyJ!");
-
-    // let unparesed_file =
-    let test: Example = parse_Example(
-        "Val,Richter\nBene,Brandmeier\nMario,Hinkel\nMaxi,Floto\nPflipper,Wolf\nTori,Gonnheimer\nSander,Stella\n",
-    )
-    .expect("u suck");
-    println!("{}", serde_json::to_string(&test).unwrap());
 
     lib::hi();
     Ok(())
