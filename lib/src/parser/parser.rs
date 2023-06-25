e6d8be64f9020de31a55e7028dba4656d8dbdb5b
sander <sander.stella00@gmail.com>
2023-06-25 11:48:03 +0200

    implented parsing for compilationunit, classdeclaration, typeJ, primitivetype
    Signed-off-by: sander <sander.stella00@gmail.com>

--- lib/src/parser/parser.rs
+++ lib/src/parser/parser.rs
@@ -2,14 +2,17 @@ extern crate pest;
 extern crate pest_derive;
 
 use super::*;
+use crate::types::{Class, Expr, FieldDecl, Type};
 use pest::error::Error;
-use pest::iterators::Pair;
+use pest::iterators::{Pair, Pairs};
 use pest::Parser;
 use pest_derive::Parser;
 use serde::{Deserialize, Serialize};
+use std::iter::Map;
+use std::path::Iter;
 
 #[derive(Parser)]
-#[grammar = "src/parser/ExampleGrammar.pest"]
+#[grammar = "src/parser/JavaGrammar.pest"]
 struct ExampleParser;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -19,23 +22,95 @@ pub enum Example {
     List(Vec<Example>),
 }
 
-pub fn parse_Example(file: &str) -> Result<Example, Error<Rule>> {
-    let example: Pair<Rule> = ExampleParser::parse(Rule::List, file)?.next().unwrap();
+pub fn parse_Programm(file: &str) -> Result<Vec<Class>, Error<Rule>> {
+    let example: Pair<Rule> = ExampleParser::parse(Rule::compilationunit, file)?
+        .next()
+        .unwrap();
 
-    Ok(parse_value(example))
+    if (example.as_rule() != Rule::compilationunit) {
+        panic!();
+    }
+    let pasedClases = example.into_inner().map(parse_class).collect();
+    Ok(pasedClases)
 }
 
-fn parse_value(pair: Pair<Rule>) -> Example {
+fn parse_class(pair: Pair<Rule>) -> Class {
     match pair.as_rule() {
-        Rule::ID => Example::ID(String::from(pair.as_str())),
-        Rule::Name => Name(pair),
-        Rule::List => Example::List(pair.into_inner().map(parse_value).collect()),
+        Rule::classdeclaration => {
+            let mut inners = pair.into_inner();
+            let name = next_id(&mut inners);
+            let mut fields = vec![];
+            let mut methods = vec![];
+            for fieldOrMethod in inners {
+                match fieldOrMethod.as_rule() {
+                    Rule::fielddeclaration => {
+                        // add to fields list
+                    }
+                    Rule::methoddeclaration => {
+                        // add to method list
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            Class {
+                name,
+                fields,
+                methods,
+            }
+        }
+        _ => todo!(),
+    }
+}
+fn next_id(inners: &mut Pairs<Rule>) -> String {
+    inners.next().unwrap().to_string()
+}
+
+fn parse_field(pair: Pair<Rule>) -> FieldDecl {
+    match pair.as_rule() {
+        Rule::fielddeclaration => {
+            let mut inners = pair.into_inner();
+            let typeJ = parse_Type(inners.next().unwrap());
+            let varDecels = vec![];
+
+            todo!()
+        }
+
+        _ => unreachable!(),
     }
 }
 
+fn parse_Type(pair: Pair<Rule>) -> Type {
+    match pair.as_rule() {
+        Rule::primitivetype => match pair.as_str() {
+            "boolean" => Type::Bool,
+            "int" => Type::Int,
+            "char" => Type::Char,
+            "String" => Type::String,
+            "void" => Type::Void,
+            "null" => Type::Null,
+            _ => unreachable!(),
+        },
+        Rule::IDENTIFIER => Type::Class(next_id(&mut pair.into_inner())),
+        _ => unreachable!(),
+    }
+}
+
+fn parse_value(pair: Pair<Rule>) -> Expr {
+    match pair.as_rule() {
+        // Rule::ID => Example::ID(String::from(pair.as_str())),
+        // Rule::Name => Name(pair),
+        // Rule::List => Example::List(pair.into_inner().map(parse_value).collect()),
+        Rule::IDENTIFIER => Expr::String(String::from(pair.as_str())),
+
+        _ => Expr::String(String::from(pair.as_str())),
+    }
+}
+/*
 pub fn Name(pair: Pair<Rule>) -> Example {
     let mut pairs = pair.into_inner();
     let a = parse_value(pairs.next().unwrap());
     let b = parse_value(pairs.next().unwrap());
     return Example::Name(Box::new(a), Box::new(b));
 }
+
+ */
--- lib/src/parser/test.rs
+++ lib/src/parser/test.rs
@@ -2,10 +2,7 @@ use super::*;
 
 #[test]
 fn testtest() {
-    let test: Example = parse_Example(
-        "Val,Richter\nBene,Brandmeier\nMario,Hinkel\nMaxi,Floto\nPflipper,Wolf\nTori,Gonnheimer\nSander,Stella\n",
-    )
-        .expect("u suck");
+    //  let test: Example = parse_Example(     "Val,Richter\nBene,Brandmeier\nMario,Hinkel\nMaxi,Floto\nPflipper,Wolf\nTori,Gonnheimer\nSander,Stella\n",).expect("u suck");
     // TODO: Use asserts or something, not println
-    println!("{}", serde_json::to_string(&test).unwrap());
+    // println!("{}", serde_json::to_string(&test).unwrap());
 }
