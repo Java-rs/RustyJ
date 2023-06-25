0f523475f77dc08bf0b731b705960a4177ad80fd
ArtInLines <linesinart@gmail.com>
2023-06-25 17:15:14 +0200

    Added new grammar for parsing

--- lib/src/parser/parser.rs
+++ lib/src/parser/parser.rs
@@ -1,29 +1,20 @@
 extern crate pest;
 extern crate pest_derive;
 
-use super::*;
-use crate::parser::Rule::name;
-use crate::types::Stmt::{Block, StmtExprStmt};
 use crate::types::{Class, Expr, FieldDecl, MethodDecl, Stmt, StmtExpr, Type};
 use pest::error::Error;
 use pest::iterators::{Pair, Pairs};
 use pest::Parser;
 use pest_derive::Parser;
-use serde::{Deserialize, Serialize};
-use std::iter::Map;
-use std::path::Iter;
-use std::ptr::null;
 
 #[derive(Parser)]
-#[grammar = "src/parser/JavaGrammar.pest"]
+#[grammar = "src/parser/NewGrammar.pest"]
 struct ExampleParser;
 
 pub fn parse_Programm(file: &str) -> Result<Vec<Class>, Error<Rule>> {
-    let example: Pair<Rule> = ExampleParser::parse(Rule::compilationunit, file)?
-        .next()
-        .unwrap();
+    let example: Pair<Rule> = ExampleParser::parse(Rule::Program, file)?.next().unwrap();
 
-    if (example.as_rule() != Rule::compilationunit) {
+    if (example.as_rule() != Rule::Program) {
         panic!();
     }
     let pasedClases = example.into_inner().map(parse_class).collect();
@@ -32,17 +23,17 @@ pub fn parse_Programm(file: &str) -> Result<Vec<Class>, Error<Rule>> {
 
 fn parse_class(pair: Pair<Rule>) -> Class {
     match pair.as_rule() {
-        Rule::classdeclaration => {
+        Rule::ClassDecl => {
             let mut inners = pair.into_inner();
             let other_name = next_id(&mut inners);
             let mut fields = vec![];
             let mut methods = vec![];
             for fieldOrMethod in inners {
                 match fieldOrMethod.as_rule() {
-                    Rule::fielddeclaration => {
+                    Rule::FieldDecl => {
                         fields.append(&mut parse_field(fieldOrMethod));
                     }
-                    Rule::methoddeclaration => {
+                    Rule::MethodDecl => {
                         methods.push(parse_method(fieldOrMethod));
                     }
                     _ => unreachable!(),
@@ -63,7 +54,7 @@ fn next_id(inners: &mut Pairs<Rule>) -> String {
 
 fn parse_method(pair: Pair<Rule>) -> MethodDecl {
     match pair.as_rule() {
-        Rule::methoddeclaration => {
+        Rule::MethodDecl => {
             let mut inners = pair.into_inner();
             let ret_type = parse_Type(inners.next().unwrap());
             let mut methoddeclarator = inners.next().unwrap().into_inner();
@@ -75,9 +66,9 @@ fn parse_method(pair: Pair<Rule>) -> MethodDecl {
                     let params1 = paramList.into_inner();
                     for param in params1 {
                         let mut inner = param.into_inner();
-                        let TypeJ = parse_Type(inner.next().unwrap());
+                        let jtype = parse_Type(inner.next().unwrap());
                         let id = next_id(&mut inner);
-                        params.push((TypeJ, id))
+                        params.push((jtype, id))
                     }
                 }
             }
@@ -96,21 +87,21 @@ fn parse_method(pair: Pair<Rule>) -> MethodDecl {
 
 fn parse_Stmt(pair: Pair<Rule>) -> Vec<Stmt> {
     match pair.as_rule() {
-        Rule::blockstatement => {
+        Rule::BlockStmt => {
             let mut inner = pair.into_inner();
             let first = inner.next().unwrap();
             match first.as_rule() {
-                Rule::typeJ => {
-                    let typeJ = parse_Type(first);
-                    let varDecels = inner.next().unwrap().into_inner();
-                    varDecels
+                Rule::JType => {
+                    let jtype = parse_Type(first);
+                    let var_decels = inner.next().unwrap().into_inner();
+                    var_decels
                         .map(|x| {
                             let mut inner = x.into_inner();
                             let other_name = next_id(&mut inner);
                             match inner.next() {
-                                None => vec![Stmt::LocalVarDecl(typeJ.clone(), other_name)],
+                                None => vec![Stmt::LocalVarDecl(jtype.clone(), other_name)],
                                 Some(expresion) => vec![
-                                    Stmt::LocalVarDecl(typeJ.clone(), other_name.clone()),
+                                    Stmt::LocalVarDecl(jtype.clone(), other_name.clone()),
                                     Stmt::StmtExprStmt(StmtExpr::Assign(
                                         other_name,
                                         parse_expr(expresion),
@@ -121,7 +112,7 @@ fn parse_Stmt(pair: Pair<Rule>) -> Vec<Stmt> {
                         .flatten()
                         .collect()
                 }
-                Rule::statement => {
+                Rule::Stmt => {
                     vec![]
                 }
                 _ => unreachable!(),
@@ -132,10 +123,6 @@ fn parse_Stmt(pair: Pair<Rule>) -> Vec<Stmt> {
 }
 fn parse_statement(pair: Pair<Rule>) -> Stmt {
     match pair.as_rule() {
-        Rule::statementwithouttrailingsubstatement => {}
-        Rule::ifthenstatement => {}
-        Rule::ifthenelsestatement => {}
-        Rule::whilestatement => {}
         _ => unreachable!(),
     }
     todo!()
@@ -145,9 +132,9 @@ fn parse_statement(pair: Pair<Rule>) -> Stmt {
 
 fn parse_field(pair: Pair<Rule>) -> Vec<FieldDecl> {
     match pair.as_rule() {
-        Rule::fielddeclaration => {
+        Rule::FieldDecl => {
             let mut inners = pair.into_inner();
-            let typeJ = parse_Type(inners.next().unwrap());
+            let JType = parse_Type(inners.next().unwrap());
             let varDecels = inners.next().unwrap().into_inner();
 
             varDecels
@@ -156,12 +143,12 @@ fn parse_field(pair: Pair<Rule>) -> Vec<FieldDecl> {
                     let other_name = next_id(&mut inner);
                     match inner.next() {
                         None => FieldDecl {
-                            field_type: typeJ.clone(),
+                            field_type: JType.clone(),
                             name: other_name,
                             val: None,
                         },
                         Some(val) => FieldDecl {
-                            field_type: typeJ.clone(),
+                            field_type: JType.clone(),
                             name: other_name,
                             val: Some(parse_expr(val)),
                         },
@@ -176,7 +163,7 @@ fn parse_field(pair: Pair<Rule>) -> Vec<FieldDecl> {
 
 fn parse_Type(pair: Pair<Rule>) -> Type {
     match pair.as_rule() {
-        Rule::primitivetype => match pair.as_str() {
+        Rule::PrimitiveType => match pair.as_str() {
             "boolean" => Type::Bool,
             "int" => Type::Int,
             "char" => Type::Char,
@@ -185,7 +172,7 @@ fn parse_Type(pair: Pair<Rule>) -> Type {
             "null" => Type::Null,
             _ => unreachable!(),
         },
-        Rule::IDENTIFIER => Type::Class(next_id(&mut pair.into_inner())),
+        Rule::Identifier => Type::Class(next_id(&mut pair.into_inner())),
         _ => unreachable!(),
     }
 }
@@ -197,7 +184,7 @@ fn parse_value(pair: Pair<Rule>) -> Expr {
         // Rule::ID => Example::ID(String::from(pair.as_str())),
         // Rule::Name => Name(pair),
         // Rule::List => Example::List(pair.into_inner().map(parse_value).collect()),
-        Rule::IDENTIFIER => Expr::String(String::from(pair.as_str())),
+        Rule::Identifier => Expr::String(String::from(pair.as_str())),
 
         _ => Expr::String(String::from(pair.as_str())),
     }
